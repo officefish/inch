48576a45bab2b9c71087acaf7a982230a35ca2c7
inozemcev (windows) <= officefish>
2021-09-25 15:53:06 +0300

    add auth reducers

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,26 @@
-import { createStore, combineReducers } from 'redux'
+// import { createStore, combineReducers } from 'redux'
 
-import titleReducer from "./reducers/titleReducer";
-import profileReducer from "./reducers/profileReducer";
+// import titleReducer from "./reducers/titleReducer";
+// import profileReducer from "./reducers/profileReducer";
 
-let reducers = combineReducers ({
-    profile:profileReducer,
-    document:titleReducer
-})
+// let reducers = combineReducers ({
+//     profile:profileReducer,
+//     document:titleReducer
+// })
 
-let store = createStore(reducers)
-export default store
+// let store = createStore(reducers)
+// export default store
+
+import {createStore, applyMiddleware} from 'redux'
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
