46f179cb3791384e333a99ddb857b423ecd73b0d
Sergey Inozemcev <futuresimple@yandex.ru>
2021-09-30 02:40:34 +0300

    app bar in progress

--- client/src/components/mui/app.mui.jsx
+++ client/src/components/mui/app.mui.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Box from '@material-ui/core/Box';
+import { fade, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    container: {
+      display:'flex',
+      justifyContent:'space-between'
+    }
+}))
 
 const App = () => {
-  return <Button color="primary">Hello World</Button>;
+  const classes = useStyles();
+
+  return <AppBar
+      position="static"
+      >
+        <div className={classes.container}>
+        <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div">
+              Inch
+            </Typography>
+        </Toolbar>
+
+        <Toolbar>
+        <Button color="inherit">Login</Button>
+        </Toolbar>
+        </div>
+            
+    </AppBar>
+  
 }
 export default App
