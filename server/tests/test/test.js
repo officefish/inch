831ce36139369cbd90ad287f97e1f21cd81742ab
inozemcev (windows) <= officefish>
2021-10-07 15:21:57 +0300

    auth api testing in progress

--- server/tests/test/test.js
+++ server/tests/test/test.js
@@ -27,6 +27,47 @@ let should = require('chai').should(),
     api = supertest('http://localhost:3001'),
     faker = require('faker')
 
+const methods = {
+    signup:'Signup'
+}
+
+const errorMessages = {
+    username: {
+        noExist: "Username field not found",
+        tooShort: "Username length should contain 6 or more symbols",
+        tooLong:"Username length should be no more than 20 symbols"
+    },
+    email: {
+        noExist:'Email field not found',
+        notValid:'Not valid email'
+    },
+    password: {
+        noExist:'Password field not found',
+        tooShort:'Password length should contain 6 or more symbols',
+        tooLong:'Password length should be no more than 40 symbols'
+    }
+}
+
+const mockValues = {
+    tooShort:"abcd",
+    tooLong:"abcdefghijklmnopqrstuvwxyz", 
+    validUsername:"techies",
+    notValidEmail:"techies@@gmail.com",
+    validEmail:"techies@gmail.com"  
+}
+
+const mockParams = {
+    noExistUsername:{},
+    tooShortUsername:{username:mockValues.tooShort},
+    tooLongUsername:{username:mockValues.tooLong},
+    noExistEmail:{username:mockValues.validUsername},
+    notValidEmail:{username:mockValues.validUsername, email:mockValues.notValidEmail},
+    noExistPassword:{username:mockValues.validUsername, email:mockValues.validEmail}
+}
+
+const directories = {
+    signup:'/api/auth/signup'
+}
 
 describe('SPA', function() {
    
@@ -40,55 +81,105 @@ describe('SPA', function() {
 
 })
 
-describe('Auth API', () => {
-
-    describe('POST /api/auth/signup ', function() {
+const noValidParamsTests = (title, directory, params, errorMsg) => {
+    const paramsStr = JSON.stringify(params)
+    const method = title + '?' + paramsStr
+    return () => {
         
-        it('Signup should return json', function(done) {
-            api.post(`/api/auth/signup`)
+        it( method + ' return a 422 status response', function(done) {
+            api.post(directory)
+                .set('Accept', 'application/json')
+                .send(params)
+                .end((err, res) => {
+                    res.should.have.status(422)
+                    done()
+                })    
+        })
+
+        it( method + ' return object with .error property', function(done) {
+            api.post(directory)
+                .set('Accept', 'application/json')
+                .send(params)
+                .end((err, res) => {
+                    res.body.should.be.a('object')
+                    res.body.should.have.property('error')
+                    done()
+                })    
+        })
+
+        it( method + ' error is: ' + errorMsg, function(done) {
+            api.post(directory)
+                .set('Accept', 'application/json')
+                .send(params)
+                .end((err, res) => {
+                    res.body.should.be.a('object')
+                    res.body.should.have.property('error')
+                        .eql(errorMsg)
+                    done()
+                })    
+        })
+
+    }
+}
+
+const shouldResponseJSONTest = (method, directory) => {
+    return () => {
+        it(method + ' should return json', function(done) {
+            api.post(directory)
                 .set('Accept', 'application/json')
                 .end((err, res) => {
                     res.headers["content-type"].should.contains('application/json')
                     done()
                 })    
         })
+    }
+}
 
-    })
+describe('Auth API', () => {
 
-    describe('POST /api/auth/signup :no params', function() {
+    describe('POST ' + directories.signup, 
+        shouldResponseJSONTest(methods.signup, directories.signup)        
+    )
 
-        it('Signup :no params return a 422 status response', function(done) {
-            api.post(`/api/auth/signup`)
-                .set('Accept', 'application/json')
-                .end((err, res) => {
-                    res.should.have.status(422)
-                    res.body.should.be.a('object')
-                    res.body.should.have.property('error')
-                        .eql('Username field not found')
-                    done()
-                })    
-        })
+    describe("POST " + directories.signup + ":" + JSON.stringify(mockParams.noExistUsername), 
+        noValidParamsTests(
+            methods.signup, 
+            directories.signup, 
+            mockParams.noExistUsername, 
+            errorMessages.username.noExist
+    ))
 
-        it("Signup :no params return object with .error property", function(done) {
-            api.post(`/api/auth/signup`)
-                .set('Accept', 'application/json')
-                .end((err, res) => {
-                    res.body.should.be.a('object')
-                    res.body.should.have.property('error')
-                    done()
-                })    
-        })
+    describe("POST " + directories.signup + ":" + JSON.stringify(mockParams.tooShortUsername), 
+        noValidParamsTests(
+            methods.signup, 
+            directories.signup, 
+            mockParams.tooShortUsername, 
+            errorMessages.username.tooShort
+    ))
 
-        it("Signup :no params error is:'Username field not found' ", function(done) {
-            api.post(`/api/auth/signup`)
-                .set('Accept', 'application/json')
-                .end((err, res) => {
-                    res.body.should.be.a('object')
-                    res.body.should.have.property('error')
-                        .eql('Username field not found')
-                    done()
-                })    
-        })
+    describe("POST " + directories.signup + ":" + JSON.stringify(mockParams.tooLongUsername), 
+        noValidParamsTests(
+            methods.signup, 
+            directories.signup, 
+            mockParams.tooLongUsername, 
+            errorMessages.username.tooLong
+    ))
 
-    })
+    describe("POST " + directories.signup + ":" + JSON.stringify(mockParams.noExistEmail), 
+        noValidParamsTests(
+            methods.signup, 
+            directories.signup, 
+            mockParams.noExistEmail, 
+            errorMessages.email.noExist
+    ))
+
+    describe("POST " + directories.signup + ":" + JSON.stringify(mockParams.notValidEmail), 
+        noValidParamsTests(
+            methods.signup, 
+            directories.signup, 
+            mockParams.notValidEmail, 
+            errorMessages.email.notValid
+    ))
+
+
 })
